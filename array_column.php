49d51d608253f32fc08dc212f687dccc0234ca14
Nikhil Kanoor <nk352@afsconnect2.njit.edu>
2016-11-19 20:13:18 -0500

    array_column

---
+++ array_column.php
@@ -0,0 +1,27 @@
+<?php
+$records = array(
+	array(
+		'id' => 2135,
+		'first_name' => 'John',
+		'last_name' => 'Doe',
+	),
+	array(
+		'id' => 3245,
+		'first_name' => 'Sally',
+		'last_name' => 'Smith',
+	),
+	array(
+		'id' => 5432,
+		'first_name' => 'Jane',
+		'last_name' => 'Jones',
+	),
+	array(
+		'id' => 5623,
+		'first_name' => 'Peter',
+		'last_name' => 'Doe',
+	),
+);
+
+$first_names = array_column($records, 'first_name');
+print_r($first_names);
+?>
